e11f73e6dd9c3b26bd2d60013aa0617e8ee0e602
rozhnevay <weic8vne>
2020-05-30 18:56:50 +0300

    hw-12

---
+++ hw-05/src/main/java/ru/otus/spring/domain/UserInfo.java
@@ -0,0 +1,59 @@
+package ru.otus.spring.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user_info")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class UserInfo implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", unique = true, nullable = false)
+  private Long id;
+
+  @Column(name = "login")
+  private String login;
+
+  @Column(name = "password")
+  private String password;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return getLogin();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
