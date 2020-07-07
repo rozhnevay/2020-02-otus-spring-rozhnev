ddb8d67c6a251ad21586b5ccce3e25d985222c06
Alexey Rozhnev <Rozhnev.A.Yur@sberbank.ru>
2020-07-07 14:31:58 +0500

    hw-17

--- hw-05/src/main/java/ru/otus/spring/repository/BookRepository.java
+++ hw-05/src/main/java/ru/otus/spring/repository/BookRepository.java
@@ -2,9 +2,7 @@ package ru.otus.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.spring.domain.Book;
 
-@RepositoryRestResource(path = "book")
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 }
--- hw-05/src/test/java/ru/otus/spring/dao/BookDaoJpaTest.java
+++ hw-05/src/test/java/ru/otus/spring/dao/BookDaoJpaTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collections;
 import java.util.HashSet;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import ru.otus.spring.domain.Author;
